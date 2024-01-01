16b70692f692128dbcb21c52170372b218eeaad9
Regis Yamamoto <regisyamamoto@gmail.com>
2024-01-01 00:53:54 -0300

    Criado migration V202312311841__create-table-parking-spot.sql

--- src/main/resources/db/migration/V202312311841__create-table-parking-spot.sql
+++ src/main/resources/db/migration/V202312311841__create-table-parking-spot.sql
@@ -1,3 +1,10 @@
 CREATE TABLE parking_spot(
-    id VARCHAR(255) PRIMARY KEY
+    id VARCHAR(36) PRIMARY KEY,
+    parking_spot_number VARCHAR(10) NOT NULL COMMENT 'Número da vaga do estacionamento',
+    license_plate VARCHAR(7) NOT NULL COMMENT 'Placa do carro',
+    car_brand VARCHAR(70) NOT NULL COMMENT 'Marca do carro',
+    car_model VARCHAR(70) NOT NULL COMMENT 'Modelo do carro',
+    car_color VARCHAR(70) NOT NULL COMMENT 'Cor do carro',
+    registration_date TIMESTAMP NOT NULL COMMENT 'Data do cadastro',
+    responsible_name VARCHAR (130) NOT NULL COMMENT 'Nome do responsavel'
 );
