57a1498c30ac411164fde3b96f03abffda14bb57
Regis Yamamoto <regisyamamoto@gmail.com>
2024-07-12 10:16:19 -0300

    Add transaction id to MDC

--- src/main/java/com/regis/parking_service/interceptor/LoggerInterceptor.java
+++ src/main/java/com/regis/parking_service/interceptor/LoggerInterceptor.java
@@ -3,9 +3,13 @@ package com.regis.parking_service.interceptor;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.MDC;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Enumeration;
+import java.util.UUID;
 
 @Slf4j
 @Component
@@ -14,6 +18,12 @@ public class LoggerInterceptor implements HandlerInterceptor {
   @Override
   public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
       throws Exception {
+    String transactionId = request.getHeader("transaction-id");
+    if (transactionId == null) {
+      transactionId = UUID.randomUUID().toString();
+    }
+    MDC.put("transactionId", transactionId);
+
     log.info("postHandle");
     return true;
   }
@@ -28,6 +38,7 @@ public class LoggerInterceptor implements HandlerInterceptor {
   @Override
   public void afterCompletion(
       HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+    MDC.clear();
     log.info("afterCompletion");
   }
 }
