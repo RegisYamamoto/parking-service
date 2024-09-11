4ba067c682bef4c60421faf94890d0b2797ca897
Regis Yamamoto <regisyamamoto@gmail.com>
2024-09-11 07:23:13 -0300

    Alterado de GetMapping para PostMapping

--- src/main/java/com/regis/parking_service/controller/EmailController.java
+++ src/main/java/com/regis/parking_service/controller/EmailController.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,7 +24,7 @@ public class EmailController {
   @Autowired
   private EmailService emailService;
 
-  @GetMapping
+  @PostMapping
   public ResponseEntity sendEmails(@RequestBody @Valid List<Email> emails)
     throws ExecutionException, InterruptedException {
     emailService.sendEmails(emails);
