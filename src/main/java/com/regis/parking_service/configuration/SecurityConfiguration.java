f230358fcd1f4c505e9650729f12d6245972ed65
Regis Yamamoto <regisyamamoto@gmail.com>
2024-10-07 19:04:12 -0300

    add requestMatchers error permite all

--- src/main/java/com/regis/parking_service/configuration/SecurityConfiguration.java
+++ src/main/java/com/regis/parking_service/configuration/SecurityConfiguration.java
@@ -28,6 +28,7 @@ public class SecurityConfiguration {
         .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
         .authorizeHttpRequests(authorize -> authorize
             .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
+            .requestMatchers("/error").permitAll() // Tem que adicionar essa permissão senão todos os erros da aplicação retornarão 403 Forbidden
             .requestMatchers(HttpMethod.POST, "/users").hasRole("ADMIN")
             .requestMatchers(HttpMethod.GET, "/actuator/**").hasRole("ADMIN")
             .requestMatchers(HttpMethod.POST, "/parking-spots").hasRole("ADMIN")
